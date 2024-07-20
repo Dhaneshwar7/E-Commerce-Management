d7f9159a532cfea9e45695deacecb20134c4888b
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-21 01:10:44 +0530

    testing v4

--- src/store/Actions/adminActions.js
+++ src/store/Actions/adminActions.js
@@ -12,20 +12,19 @@ import {
 
 export const asyncHomepage = () => async (dispatch, getState) => {
 	try {
-		const { data } = await axiosInstance.get('/');
-		console.log(data);
-		dispatch(setAdmin(data));
+		const data = await axiosInstance.get('/');
+		// console.log(data);
+		dispatch(setAdmin(data.data.currentAdmin));
 	} catch (error) {
-		console.log(error.response.data.message);
 		dispatch(isError(error.response.data.message));
 	}
 };
 /* -----------  CURRENT ADMIN   -----------*/
 export const asyncCurrentAdmin = () => async (dispatch, getState) => {
 	try {
-		const { data } = await axiosInstance.get('/admin');
-		console.log(data, 'Current Admin Added');
-		dispatch(setAdmin(data));
+		const data = await axiosInstance.get('/admin');
+		// console.log(data.data.currentAdmin, 'Current Admin Added');
+		dispatch(setAdmin(data.data.currentAdmin));
 	} catch (error) {
 		console.log(error.response.data.message);
 		dispatch(isError(error.response.data.message));
@@ -36,7 +35,7 @@ export const asyncSignUpAdmin = admin => async (dispatch, getState) => {
 	try {
 		console.log(admin);
 		const { data } = await axiosInstance.post('/admin/signup', admin);
-		console.log(data, 'Admin SIGN_UP done');
+		// console.log(data, 'Admin SIGN_UP done');
 		dispatch(asyncCurrentAdmin());
 	} catch (error) {
 		console.log(error.response.data.message);
@@ -47,7 +46,7 @@ export const asyncSignUpAdmin = admin => async (dispatch, getState) => {
 /* -----------   ADMIN SIGN_IN   ----------*/
 export const asyncSignInAdmin = admin => async (dispatch, getState) => {
 	try {
-		const { data } = await axiosInstance.post('/admin/signin', admin);
+		const data = await axiosInstance.post('/admin/signin', admin);
 		console.log(data, 'Admin SIGN_IN done');
 		dispatch(asyncCurrentAdmin(data));
 	} catch (error) {
@@ -60,10 +59,10 @@ export const asyncSignInAdmin = admin => async (dispatch, getState) => {
 export const asyncLogoutAdmin = () => async (dispatch, getState) => {
 	try {
 		const { data } = await axiosInstance.get('/admin/signout');
-		console.log(data, 'Admin Logout-done!');
+		// console.log(data, 'Admin Logout-done!');
 		dispatch(removeAdmin(data));
 	} catch (error) {
-		console.log(error.response.data.message);
+		// console.log(error.response.data.message);
 		dispatch(isError(error.response.data.message));
 	}
 };
